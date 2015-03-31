71900cdacf40981959e831f74315263693697cf1
Dennis Hughes <miyahira@miyahira.me>
2015-03-31 16:36:28 +0900

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+  key: "YOUR_CW_API_KEY_HERE",
+}
+
+module.exports = config
---
+++ qjob.js
@@ -0,0 +1,26 @@
+var cwapi = require('cwapi')
+var config = require("./config.js")
+var _ = require("underscore")
+
+cwapi.token = config.key
+
+
+cwapi.getRooms(function(data, res){
+  rooms = data
+  // filter non groups (DMs) out
+  rooms = _.filter(rooms, function(room){return room.type == "group"})
+  rooms = _.sortBy(rooms, "room_id")
+
+  rooms = _.map(rooms, function(member){
+    return _.pick(member, ["room_id", "name"])
+  })
+  console.log(rooms)
+
+  //leave all rooms
+//  _.each(rooms, function(room){
+//    cwapi.leaveRoom(room.room_id, function(data, res){
+//      console.log(room + " 退出完了")
+//    })
+//  })
+
+})
